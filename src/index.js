0d200400a34ee1a4764b4b949068c819421d788b
Rohit Rathod <122153964+rohitrath0d@users.noreply.github.com>
2025-07-14 11:00:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,5 +31,32 @@ app.use('/api/chatroom', chatroomRoutes)
 app.use('/api/subscription', stripeSubscriptionRoutes)
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
+
+// merging both app service and messqge queue service to deploy as combined.
+app.listen(PORT, async () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+
+  await connectRabbit();
+
+  const channel = getChannel();
+  const QUEUE_NAME = 'gemini-verification';
+
+  await channel.assertQueue(QUEUE_NAME);
+
+  // Consume messages from queue here
+  channel.consume(QUEUE_NAME, async (msg) => {
+    if (msg !== null) {
+      const data = JSON.parse(msg.content.toString());
+      console.log('📥 Received from RabbitMQ:', data);
+
+      // TODO: Process the message as needed
+      // e.g., verify image, update DB, notify user...
+
+      channel.ack(msg);
+    }
+  });
+
+  console.log(`🛠️ RabbitMQ consumer listening on queue "${QUEUE_NAME}"`);
+});
